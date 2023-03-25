172b7c1e2de4b7288019dce9dc1fca5a3aac42e7
Abdjalil Brihoum <ou.brihoum@gmail.com>
2023-03-25 17:03:20 +0100

    fix animations

--- src/components/Home9/Home9.tsx
+++ src/components/Home9/Home9.tsx
@@ -4,16 +4,18 @@ export default function Home9() {
   return (
     <>
       <section className={styles["main-block"]}>
-        <Box bgColor="#eee">
-          <Box.Heading>THERE’S MORE TO COME</Box.Heading>
-          <Box.SubHeading>
-            <>
-              The Limited Test only evaluates a subset of Counter-Strike 2's features, so that major issues can be
-              resolved before the summer. But there’s much more to come. In just a few months we’ll reveal all of the
-              details of Counter-Strike 2, and we can’t wait to share it with you.
-            </>
-          </Box.SubHeading>
-        </Box>
+        <div className={styles["animation"]} data-scroll>
+          <Box bgColor="#eee">
+            <Box.Heading>THERE’S MORE TO COME</Box.Heading>
+            <Box.SubHeading>
+              <>
+                The Limited Test only evaluates a subset of Counter-Strike 2's features, so that major issues can be
+                resolved before the summer. But there’s much more to come. In just a few months we’ll reveal all of the
+                details of Counter-Strike 2, and we can’t wait to share it with you.
+              </>
+            </Box.SubHeading>
+          </Box>
+        </div>
       </section>
     </>
   );
